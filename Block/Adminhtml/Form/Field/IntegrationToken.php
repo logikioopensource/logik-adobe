89cd7535ba789052ced7d494ade45a4e61fb2052
iandrews <idrews094@gmail.com>
2025-04-30 11:44:56 -0700

    merged addToCart and LogikSettings

---
+++ Block/Adminhtml/Form/Field/IntegrationToken.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Logik\Logik\Block\Adminhtml\Form\Field;
+
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class IntegrationToken extends Field
+{
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        $html = '<div class="integration-token-wrapper">';
+        $html .= '<input type="password" id="' . $element->getHtmlId() . '" 
+                    name="' . $element->getName() . '"
+                    class="integration-token-input" 
+                    value="' . $element->getEscapedValue() . '" 
+                    readonly="readonly">';
+        $html .= '<button type="button" 
+                    class="action-default scalable toggle-token" 
+                    data-mage-init=\'{"Logik_LogikSettings/js/toggle-token": {}}\'>
+                    <span>Show Token</span>
+                </button>';
+        $html .= '</div>';
+
+        return $html;
+    }
+
+    protected function _renderScopeLabel(AbstractElement $element)
+    {
+        return '';
+    }
+} 
