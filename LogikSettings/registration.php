bd510e32c09da057ce9083fbc70c1594b8d6a62a
iandrews <idrews094@gmail.com>
2025-04-30 10:46:15 -0700

    composer setup

--- LogikSettings/registration.php
+++ LogikSettings/registration.php
@@ -1,5 +1,13 @@
 <?php
+/**
+ * Copyright © Logik, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Logik_LogikSettings', __DIR__);
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Logik_LogikSettings',
+    __DIR__
+);
