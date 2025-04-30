de97e37637d62e298de4dcfc6011e8807422e972
iandrews <idrews094@gmail.com>
2025-04-30 14:24:01 -0700

    add requirejs file

---
+++ view/adminhtml/requirejs-config.js
@@ -0,0 +1,11 @@
+var config = {
+    map: {
+        '*': {
+            'Logik_Logik/js/toggle-token': 'Logik_Logik/js/toggle-token',
+            'Logik_Logik/js/test-credentials': 'Logik_Logik/js/test-credentials'
+        }
+    },
+    deps: [
+        'jquery'
+    ]
+}; 
