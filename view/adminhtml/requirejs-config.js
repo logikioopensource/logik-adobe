cc72ea5e8bc4f5c859c3b13910e74759e6c5f648
iandrews <idrews094@gmail.com>
2025-04-30 15:39:00 -0700

    fixes to routes page

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -2,7 +2,8 @@ var config = {
     map: {
         '*': {
             'Logik_Logik/js/toggle-token': 'Logik_Logik/js/toggle-token',
-            'Logik_Logik/js/test-credentials': 'Logik_Logik/js/test-credentials'
+            'Logik_Logik/js/test-credentials': 'Logik_Logik/js/test-credentials',
+            'Logik_Logik/js/form-validation': 'Logik_Logik/js/form-validation'
         }
     },
     deps: [
