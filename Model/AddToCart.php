2b08a6c9ac95f74b226e06662a813da8e22aa088
Daniel <dbrowne@logik.io>
2025-05-01 12:34:04 -0500

    Add configurable product support

--- Model/AddToCart.php
+++ Model/AddToCart.php
@@ -1,9 +1,10 @@
 <?php
-namespace Logik\Logik\Model;
+namespace Logik\AddToCart\Model;
 
-use Logik\Logik\Api\AddToCartInterface;
+use Logik\AddToCart\Api\AddToCartInterface;
 use Magento\Quote\Api\CartRepositoryInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable;
 use Psr\Log\LoggerInterface;
 use function PHPUnit\Framework\isNull;
 
@@ -12,20 +13,24 @@ class AddToCart implements AddToCartInterface
     protected $cartRepository;
     protected $logger;
     protected $productRepository;
+    protected $configurable;
 
     public function __construct(
         CartRepositoryInterface $cartRepository,
         LoggerInterface $logger,
-        ProductRepositoryInterface $productRepository
+        ProductRepositoryInterface $productRepository,
+        Configurable $configurable,
     ) {
         $this->cartRepository = $cartRepository;
         $this->logger = $logger;
         $this->productRepository = $productRepository;
+        $this->configurable = $configurable;
     }
 
     public function addItems(int $quoteId, array $items): array
     {
         $errors = [];
+        /** @var \Magento\Quote\Model\Quote */
         $quote = $this->cartRepository->getActive($quoteId);
         foreach ($items as $item) {
             try {
@@ -33,25 +38,31 @@ class AddToCart implements AddToCartInterface
                 $sku = $item->getSku();
                 $price = $item->getPrice();                
 
-                $product = $this->productRepository->get($sku);
-                // Handle Custom Options: This may need extending to add Configurable/Bundle/Grouped products with options
+                // Get Custom Options array
                 $options = [];
                 if ($item->getProductOption() !== null && $item->getProductOption()->getExtensionAttributes() !== null) {
                     foreach ($item->getProductOption()->getExtensionAttributes()->getCustomOptions() as $customOption) {
                         $options[$customOption->getOptionId()] = $customOption->getOptionValue();
                     }
                 }
-                // Specify information specific to the cart Item (other than price)
-                $request = new \Magento\Framework\DataObject([
-                    'qty' => $item->getQty(),
-                    'options' => $options
-                ]);
 
-                // Add the product to quote, getting the added $quoteItem
-                $quoteItem = $quote->addProduct($product, $request);
+                /** @var $product \Magento\Catalog\Api\Data\ProductInterface */
+                $product = $this->productRepository->get($sku);
+
+                $configurableParentIds = $this->configurable->getParentIdsByChild($product->getId());
+
+                /** @var \Magento\Quote\Model\Quote\Item */
+                $quoteItem;
+                if (!empty($configurableParentIds)) {
+                    $parentId = $configurableParentIds[0];
+                    $parentProduct = $this->productRepository->getById($parentId);
+                    $quoteItem = $this->addConfigurableProduct($product, $parentProduct, $quote, $options, $item);
+                } else {
+                    $quoteItem = $this->addSimpleProduct($product, $quote, $options, $item);
+                }
                 // Handle errors adding product
                 if (!($quoteItem instanceof \Magento\Quote\Model\Quote\Item)) {
-                    // This syntax is kinda ridiculous - why is this how to append to an array
+                    // This syntax is kinda ridiculous - why does this append to an array
                     $errors[] = [
                         'sku' => $sku,
                         'message' => 'Failed to add product to quote with message ' . $quoteItem
@@ -84,5 +95,63 @@ class AddToCart implements AddToCartInterface
             } 
         }
         return $errors;
+    }
+
+        /**
+     * Adds a configurable product to the given quote, returning the quoteItem (or string in error cases)
+     * Adding an item can also throw - we handle thrown errors and string returns essentially the
+     * 
+     * @param \Magento\Catalog\Api\Data\ProductInterface $product
+     * @param \Magento\Catalog\Api\Data\ProductInterface $parentProduct
+     * @param \Magento\Quote\Model\Quote  $quote
+     * @param [] $options
+     * @param \Magento\Quote\Api\Data\CartItemInterface $item
+     * @return \Magento\Quote\Model\Quote\Item|string
+     */
+    private function addConfigurableProduct($product, $parentProduct, $quote, $options, $item): \Magento\Quote\Model\Quote\Item {
+        /** @var \Magento\ConfigurableProduct\Model\Product\Type\Configurable $typeInstance */
+        $typeInstance = $parentProduct->getTypeInstance();
+        $configurableAttributes = $typeInstance->getConfigurableAttributes($parentProduct);
+
+        $superAttribute = [];
+
+        foreach ($configurableAttributes as $attribute) {
+            $attributeId = $attribute->getAttributeId(); // internal ID
+            $attributeCode = $attribute->getProductAttribute()->getAttributeCode(); // like 'color', 'size', etc.
+
+            // Get the value that the child (simple product) has for this attribute
+            $value = $product->getData($attributeCode);
+            
+            if ($value !== null) {
+                $superAttribute[$attributeId] = $value;
+            }
+        }
+        $request = new \Magento\Framework\DataObject([
+            'qty' => $item->getQty(),
+            'options' => $options,
+            'super_attribute' => $superAttribute
+        ]);
+        return $quote->addProduct($parentProduct, $request);
+    }
+
+    /**
+     * Adds a simple product to the given quote, returning the quoteItem (or string in error cases)
+     * Adding an item can also throw - we handle thrown errors and string returns essentially the
+     * 
+     * @param \Magento\Catalog\Api\Data\ProductInterface $product
+     * @param \Magento\Quote\Model\Quote  $quote
+     * @param [] $options
+     * @param \Magento\Quote\Api\Data\CartItemInterface $item
+     * @return \Magento\Quote\Model\Quote\Item|string
+     */
+    private function addSimpleProduct($product, $quote, $options, $item) {
+        $request = new \Magento\Framework\DataObject([
+            'qty' => $item->getQty(),
+            'options' => $options,
+        ]);
+
+        // Add the product to quote, getting the added $quoteItem
+        /** @var \Magento\Quote\Model\Quote\Item */
+        return $quote->addProduct($product, $request);
     }
 }
