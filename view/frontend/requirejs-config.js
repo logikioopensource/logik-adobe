8ab0144609a385ea54c3d3c23e34d8c8f7d1caa8
iandrews <idrews094@gmail.com>
2025-05-13 15:28:48 -0700

    fix naming inconsistency

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,8 +1,10 @@
 var config = {
     map: {
         '*': {
-            'Logik_Integration/js/logik-configurator': 'Logik_Integration/js/logik-configurator',
-            'Logik_Integration/template/logik-configurator': 'Logik_Integration/template/logik-configurator'
+            'Logik_Integration/js/logik-configurator': 'Logik_Integration/js/logik-configurator'
         }
+    },
+    paths: {
+        'Logik_Integration/template/logik-configurator': 'Logik_Integration/template/logik-configurator'
     }
 }; 
